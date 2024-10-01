5d712637d83431e6136b8069dbdde1c2cf68c243
bulklc <chris.b.logston@gmail.com>
2024-09-30 21:54:57 -0700

    deleted:    a 	new file:   docs/3D_modeling/chicken_coop.pdf 	new file:   docs/3D_modeling/lofted_bed_frame.pdf 	new file:   docs/3D_modeling/night_stand.pdf 	new file:   docs/3D_modeling/peltier_cooler.pdf 	new file:   docs/3D_modeling/pump_enclosure.pdf 	new file:   docs/3D_modeling/shed.pdf 	new file:   docs/3D_modeling/shelves_001.pdf 	new file:   docs/PE_materials/effective_seismic_weight.pdf 	new file:   docs/PE_materials/horizontal_curve.pdf 	new file:   docs/grad_assignments/CEGE_4352_Final_Presentation_V1.pdf 	new file:   docs/grad_assignments/CEGE_4352_P04_line_sinks.pdf 	new file:   docs/grad_assignments/CEGE_4501_05_HEC_HMS.pdf 	new file:   docs/grad_assignments/CEGE_4511_HW04_labyrinth_weir.pdf 	new file:   docs/grad_assignments/CEGE_4511_HW05_stilling_basins.pdf 	new file:   docs/grad_assignments/CEGE_4511_HW06_tainer_gates.pdf 	new file:   docs/grad_assignments/CEGE_4511_HW07_scour.pdf 	new file:   docs/grad_assignments/CEGE_4511_final.pdf 	new file:   docs/grad_assignments/CEGE_8351_Project_3_hodograph_method.pdf 	new file:   docs/misc/kannada_manual.pdf 	new file:   docs/misc/open_channel_calcs.pdf 	new file:   docs/misc/solving_for_frication_factor.pdf 	new file:   docs/misc/volume_of_a_solid.pdf 	new file:   docs/physics_books/physics_manual_cameroon_F03.pdf 	new file:   docs/physics_books/physics_manual_guinea_N09.pdf 	new file:   docs/physics_books/physics_manual_guinea_N10.pdf 	new file:   docs/physics_books/physics_manual_liberia_university.pdf 	new file:   docs/pure_paani/pure_paani_detail_and_assembly_01.00.pdf 	new file:   docs/pure_paani/pure_paani_detail_and_assembly_01.01.pdf 	new file:   docs/pure_paani/pure_paani_detail_and_assembly_03.01.pdf 	new file:   docs/pure_paani/pure_paani_detail_and_assembly_04.00.pdf 	new file:   docs/pure_paani/pure_paani_detail_and_assembly_05.00.pdf 	new file:   docs/pure_paani/pure_paani_detail_and_assembly_05.01.pdf 	new file:   docs/pure_paani/pure_paani_detail_and_assembly_05.02.pdf 	new file:   docs/pure_paani/pure_paani_detail_and_assembly_06.00.pdf 	new file:   favicon.png 	new file:   files.js 	new file:   index.html 	new file:   styles.css

---
+++ files.js
@@ -0,0 +1,71 @@
+const document_list = [
+  {
+    label: "3D Modeling",
+    name: "3D_modeling",
+    files: [
+      "chicken_coop.pdf",
+      "lofted_bed_frame.pdf",
+      "night_stand.pdf",
+      "pump_enclosure.pdf",
+      "shed.pdf",
+      "shelves_001.pdf",
+      "peltier_cooler.pdf"
+    ],
+  },
+  {
+    label: "Grad Assignments",
+    name: "grad_assignments",
+    files: [
+      "CEGE_4352_Final_Presentation_V1.pdf",
+      "CEGE_4352_P04_line_sinks.pdf",
+      "CEGE_4501_05_HEC_HMS.pdf",
+      "CEGE_4511_final.pdf",
+      "CEGE_4511_HW04_labyrinth_weir.pdf",
+      "CEGE_4511_HW05_stilling_basins.pdf",
+      "CEGE_4511_HW06_tainer_gates.pdf",
+      "CEGE_4511_HW07_scour.pdf",
+      "CEGE_8351_Project_3_hodograph_method.pdf",
+    ],
+  },
+  {
+    label: "Misc.",
+    name: "misc",
+    files: [
+      "kannada_manual.pdf",
+      "open_channel_calcs.pdf",
+      "solving_for_frication_factor.pdf",
+      "volume_of_a_solid.pdf",
+    ],
+  },
+  {
+    label: "PE Materials",
+    name: "PE_materials",
+    files: ["effective_seismic_weight.pdf", "horizontal_curve.pdf"],
+  },
+  {
+    label: "Physics Books",
+    name: "physics_books",
+    files: [
+      "physics_manual_cameroon_F03.pdf",
+      "physics_manual_guinea_F10.pdf",
+      "physics_manual_guinea_N09.pdf",
+      "physics_manual_liberia_university.pdf",
+    ],
+  },
+  {
+    label: "Water Filter Models",
+    name: "pure_paani",
+    files: [
+      "pure_paani_detail_and_assembly_01.00.pdf",
+      "pure_paani_detail_and_assembly_01.01.pdf",
+      "pure_paani_detail_and_assembly_03.01.pdf",
+      "pure_paani_detail_and_assembly_04.00.pdf",
+      "pure_paani_detail_and_assembly_05.00.pdf",
+      "pure_paani_detail_and_assembly_05.01.pdf",
+      "pure_paani_detail_and_assembly_05.02.pdf",
+      "pure_paani_detail_and_assembly_06.00.pdf",
+    ],
+  },
+];
+
+// export default document_list;
