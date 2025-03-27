28dad0dbe52022948e20a70cc5773ab74b3dc488
bulklc <chris.b.logston@gmail.com>
2025-03-26 21:53:57 -0700

    a

--- cards/messages_content.js
+++ cards/messages_content.js
@@ -210,7 +210,7 @@ card_messages = {
     content: {
       "letter-body": [
         { element_type: "p", content: "Dearest R" },
-        { element_type: "p", content: "003" },
+        { element_type: "p", content: "005" },
       ],
       "letter-footer": [
         { element_type: "p", content: "Best," },
